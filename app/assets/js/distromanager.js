032ea374f9889f89201b1b5cbbcb76f7a100bc4a
shimonsolo <shimonsolo@hotmail.com>
2024-11-13 23:06:51 -0300

    Updated translation to spanish and added new loading screen

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -3,8 +3,8 @@ const { DistributionAPI } = require('helios-core/common')
 const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
-// exports.REMOTE_DISTRO_URL = 'http://66.187.7.41/launcher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://66.187.7.41/launcher/distribution.json'
+// exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://helios-files.geekcorner.eu.org/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
